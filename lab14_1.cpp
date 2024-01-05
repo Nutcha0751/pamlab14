36e2e7017e67e06c452f1001bb117f3594b9d406
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-01-05 16:07:08 +0700

    lab14_1complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,7 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i = 1; i < N; i++){
+		for(int j = i; j > 0; j--){
+			if(d[j] > d[j-1]){
+				T temp = d[j];
+				d[j] = d[j-1];
+				d[j-1] = temp;
+			}
+		}
+		cout << "Pass " << i << ":";
+		for(int x = 0; x < N; x++) cout << d[x] << " ";
+		cout << endl;
+	}
 }
 
 int main(){
